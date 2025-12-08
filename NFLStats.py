82b3fb03a8f1b0e0857df836ef4e0d0149a111fe
efoster930-sketch <efoster930@phm.k12.in.us>
2025-12-08 08:35:18 -0500

    Update NFLStats.py

--- NFLStats.py
+++ NFLStats.py
@@ -5,3 +5,11 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import random
 import seaborn as sns
+import tensorflow import keras
+from keras import layers
+
+
+Offense = pd.read_csv("Offense.csv")
+Defense = pd.read_csv("Defense.csv")
+SOS = pd.read_csv("SOS.csv")
+Turnovers = pd.read_csv("Turnovers.csv")
